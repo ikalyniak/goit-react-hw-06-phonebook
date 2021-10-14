0c0e9d09a76e72f9c77abbe4bfbe7a0a97397247
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-10-14 03:08:06 +0300

    middleware logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,22 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+
+import reducer from './reducers';
+
+const middleware = [...getDefaultMiddleware(), logger];
+
+const store = configureStore({
+  reducer: {
+    contacts: reducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export default store;
+
+/** REDUX CORE -----------------------------------------------
+ 
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
@@ -9,4 +28,6 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, composeWithDevTools());
 
-export default store;
+export default store; 
+ 
+ */
